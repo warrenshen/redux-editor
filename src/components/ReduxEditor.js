4d6a77f385bfd8c2c4906485eb53eb003c623739
warrenshen <warrenzshen@gmail.com>
2016-07-17 18:47:44 -0700

    Configure project with redux

---
+++ src/components/ReduxEditor.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from '../reducers';
+
+import EditTab from './EditTab';
+
+const store = createStore(rootReducer);
+
+export default function() {
+  return (
+    <Provider store={store}>
+      <EditTab />
+    </Provider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ReduxEditor from './ReduxEditor';
+import ReduxEditor from './components/ReduxEditor';
 
 if (module.hot) {
   module.hot.accept();
