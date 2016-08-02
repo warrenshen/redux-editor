1f6873015cd9b1df539c35dae4450cf831ad5968
warrenshen <warrenzshen@gmail.com>
2016-08-01 22:10:35 -0700

    Create render caret method in walker helper

--- src/helpers/walker.js
+++ src/helpers/walker.js
@@ -55,6 +55,47 @@ class Walker {
       start: anchorCaret,
     };
   }
+
+  renderCaret(caret, node) {
+    if (caret.active) {
+      const parentNode = node.childNodes[caret.section].childNodes[caret.block];
+      const childrenNodes = parentNode.childNodes;
+
+      let baseNode = null;
+      for (let i = 0; i < childrenNodes.length && baseNode === null; i += 1) {
+        if (childrenNodes[i].isContentEditable) {
+          baseNode = childrenNodes[i];
+        }
+      }
+
+      if (baseNode !== null) {
+        baseNode.focus();
+        const selection = window.getSelection();
+        const range = document.createRange();
+
+        let offset = caret.offset;
+        let currentNode = baseNode;
+        if (offset > 0) {
+          const walker = this.createTreeWalker(baseNode);
+          while (walker.nextNode() && offset >= 0) {
+            currentNode = walker.currentNode;
+            if (offset - currentNode.length <= 0) {
+              range.setStart(currentNode, offset);
+              range.setEnd(currentNode, offset);
+            }
+            offset -= currentNode.length;
+          }
+        } else {
+          range.setStart(currentNode, 0);
+          range.setEnd(currentNode, 0);
+        }
+
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }
+    }
+  }
 }
 
 export default new Walker();
